0abf5a16c29f5d43f7b9a32ea72cff858cc71141
Venkatarao Nannapaneni <venkatcivil108@gmail.com>
2023-11-28 15:55:33 -0600

    eks-terraform file updated with available subnets

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -38,7 +38,13 @@ resource "aws_eks_cluster" "example" {
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.public.ids
+    subnet_ids = [
+      data.aws_subnet.public.ids["us-east-1a"],
+      data.aws_subnet.public.ids["us-east-1b"],
+      data.aws_subnet.public.ids["us-east-1c"],
+      data.aws_subnet.public.ids["us-east-1d"],
+      data.aws_subnet.public.ids["us-east-1f"],
+    ]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
