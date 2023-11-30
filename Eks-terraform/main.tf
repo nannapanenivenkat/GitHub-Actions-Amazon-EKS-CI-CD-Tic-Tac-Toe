c9514cbcbec89bf769aa10cd85bd2d76c954eef8
Ubuntu <ubuntu@ip-172-31-94-231.ec2.internal>
2023-11-30 02:07:32 +0000

    files updated

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -38,7 +38,11 @@ resource "aws_eks_cluster" "example" {
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.public.ids
+    subnet_ids = [
+      data.aws_subnet.public_a.id,
+      data.aws_subnet.public_b.id,
+      data.aws_subnet.public_c.id,
+    ]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
